d1d2f85767075f83eac2be95d9f96e055f653212
Bala-Magesh <balamageshkumars@gmail.com>
2024-08-14 22:40:09 +0530

    added signup, forgotpassword, home pages. Added browser router

--- ai-skill-navigator/src/App.js
+++ ai-skill-navigator/src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 
 import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
+import ForgotPassword from "./Components/ForgotPassword";
+import Home from "./Components/Home";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Login />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- ai-skill-navigator/src/Components/Login/index.js
+++ ai-skill-navigator/src/Components/Login/index.js
@@ -1,12 +1,65 @@
+// Go to /login in browser to view this page!
+
 import "./index.css";
 
 import { Component } from "react";
 
+import { Navigate } from "react-router-dom";
+
 class Login extends Component {
+  state = {
+    shouldRedirectHome: false,
+    shouldRedirectSignup: false,
+    shouldRedirectForgotPassword: false,
+  };
+
+  onSubmitLoginForm = (event) => {
+    event.preventDefault();
+  };
+
+  onClickLoginButton = () => {
+    if (this.isUserAuthenticated()) {
+      this.setState({ shouldRedirectHome: true });
+    } else {
+      //Add logic to get appropriate error msg from firebase and display
+    }
+  };
+
+  isUserAuthenticated = () => {
+    //Add logic for authentication
+    //'true' is assumed temporatily
+    return true;
+  };
+
+  onClickSignupButton = () => {
+    this.setState({ shouldRedirectSignup: true });
+  };
+
+  onClickForgotPassword = () => {
+    this.setState({ shouldRedirectForgotPassword: true });
+  };
+
   render() {
+    const {
+      shouldRedirectHome,
+      shouldRedirectSignup,
+      shouldRedirectForgotPassword,
+    } = this.state;
+    if (shouldRedirectSignup) {
+      return <Navigate to="/signup" />;
+    }
+
+    if (shouldRedirectHome) {
+      return <Navigate to="/home" replace />;
+    }
+
+    if (shouldRedirectForgotPassword) {
+      return <Navigate to="/forgotpassword" />;
+    }
+
     return (
       <div className="bg-container">
-        <form className="form-container">
+        <form onSubmit={this.onSubmitLoginForm} className="form-container">
           <h1>Login</h1>
           <label className="label-text" htmlFor="usernameInput">
             Username
@@ -20,11 +73,17 @@ class Login extends Component {
             id="passwordInput"
             type="password"
           ></input>
-          <button className="login-button">Login</button>
-          <button className="signup-button">Sign Up</button>
-          <a className="forgot-password">Forgot Password?</a>
+          <button onClick={this.onClickLoginButton} className="login-button">
+            Login
+          </button>
+          <button onClick={this.onClickSignupButton} className="signup-button">
+            Sign Up
+          </button>
+          <a onClick={this.onClickForgotPassword} className="forgot-password">
+            Forgot Password?
+          </a>
           <h1>Other Options</h1>
-          <button>Login with google</button>
+          <button>Continue with Google</button>
         </form>
       </div>
     );
